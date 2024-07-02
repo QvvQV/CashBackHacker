fff20285393412ccf7b77301113aca0eb8434587
Vadim <vadimgol07@inbox.ru>
2024-07-02 20:37:29 +0300

    first test on JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,60 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void ifAmount0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+        @Test
+    public void ifAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+        @Test
+    public void ifAmountFirstBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+        @Test
+    public void ifAmountAverageBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+        @Test
+    public void ifAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+        @Test
+    public void ifAmountEqualBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+  
+}
